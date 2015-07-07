4d33e64243e171440b9ef27ad215bb970985cee9
Stelian Morariu <stelian.morariu@ropardo.ro>
2015-07-07 13:38:36 +0300

    Initial import

---
+++ app/src/androidTest/java/com/stelianmorariu/wearme/ApplicationTest.java
@@ -0,0 +1,24 @@
+/*
+ *
+ *  *
+ *  * Copyright (c) 2015 Stelian Morariu
+ *  *
+ *  * The above copyright notice and this permission notice shall be included in all
+ *  * copies or substantial portions of the Software.
+ *  *
+ *
+ */
+
+package com.stelianmorariu.wearme;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
